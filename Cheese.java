c9823d88fb25b697a2ae1af61c1359e990d852e6
natethegreat34 <43049313+natethegreat34@users.noreply.github.com>
2019-02-10 14:16:10 -0500

    More fixing solve

---
+++ Cheese.java
@@ -0,0 +1,7 @@
+public class Cheese{
+public static void main(String[] args) {
+    QueenBoard bod = new QueenBoard (4);
+    System.out.println(bod.toString());
+    System.out.println(bod.countSolutions());
+}
+}
