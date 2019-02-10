e759fa2e22f49ab6961fe2350ca2c319ebe9046c
natethegreat34 <43049313+natethegreat34@users.noreply.github.com>
2019-02-10 13:18:18 -0500

    Still fixing (checks mean these are fully correct)

--- QueenBoard.java
+++ QueenBoard.java
@@ -1,51 +1,96 @@
 public class QueenBoard{
     private int[][]board;
     private int max;
+
     public QueenBoard(int size){
         max = size;
+        board = new int [max][max];
     }
+    public void setup (){
+        for (int a = 0; a < max; a++){
+          for (int b = 0; b < max; b++){
+              board [a][b] = 0;
+    }}}
+    //      /
+    //     /
+    // \  /
+    //  v
     private boolean addQueen(int r, int c){
         if (board [r] [c] != 0) {
             return false;
         }
         return true;
     }
+    //      /
+    //     /
+    // \  /
+    //  v
     private void adder (int r, int c){
         board [r] [c] = -1;
         //  >/
         for (int i = 0; c + i < max && r- i >= 0; i ++){
-            board [r - i] [c + i] = 1;
+            if (board [r - i] [c + i] >= 0){
+            board [r - i] [c + i] += 1;
+            }
         }
         // <\
         for (int i = 0; c- i >= 0 && r- i >= 0; i ++){
-            board [r- i] [c- i] = 1;
+            if (board [r - i] [c - i] >= 0){
+            board [r- i] [c- i] += 1;
+            }
         }
         // >\
         for (int i = 0; c+ i < max && r+ i < max; i ++){
-            board [r+ i] [c+ i] = 1;
+            if (board [r + i] [c + i] >= 0){
+            board [r+ i] [c+ i] += 1;
+            }
         }
         // </
         for (int i = 0; c- i >= 0 && r+ i < max; i ++){
-            board [r + i] [c - i] = 1;
+            if (board [r + i] [c - i] >= 0){
+            board [r + i] [c - i] += 1;
+            }
         }
         //  >
         for (int i = 0; r+ i < max; i ++){
-            board [r + i] [c] = 1;
+            if (board [r + i] [c] >= 0){
+            board [r + i] [c] += 1;
+            }
         }
         // <
         for (int i = 0; r - i >= 0 ; i ++){
-            board [r - i] [c] = 1;
+            if (board [r - i] [c] >= 0){
+            board [r - i] [c] += 1;
+            }
         }
         // v
         for (int i = 0; c - i >= 0; i ++){
-            board [r] [c - i] = 1;
+            if (board [r] [c - i] >= 0){
+            board [r] [c - i] += 1;
+            }
         }
         // ^
         for (int i = 0; c + i < max; i ++){
-            board [r] [c + i] = 1;
+            if (board [r] [c + i] >= 0){
+            board [r] [c + i] += 1;
+            }
         }
     }
+    //      /
+    //     /
+    // \  /
+    //  v
   private boolean removeQueen(int r, int c){
+      if (board [r] [c] >= 0) {
+          return false;
+      }
+      return true;
+  }
+  //      /
+  //     /
+  // \  /
+  //  v
+    private void remover (int r, int c){
       //  >/
       for (int i = 0; c + i < max && r- i >= 0; i ++){
           board [r - i] [c + i] = board [r - i] [c + i]  - 1 ;
@@ -79,7 +124,6 @@ public class QueenBoard{
           board [r] [c + i] = board [r - i] [c + i]  - 1 ;
       }
       board [r][c] = 0;
-      return true;
   }
 /**
   *@return The output string formatted as follows:
@@ -134,31 +178,29 @@ public class QueenBoard{
       if (addQueen(y, x)){
           adder(y, x);
           added = true;
-          solve (0, x + 1);
+          return solve (0, x + 1);
       }
       if (y == max && x == max && findcolQ (x) < 0){
           if (lastone){
-              for (int a = 0; a < board.length; a++){
-                for (int b = 0; b < board[y].length; b++){
-                    board [a][b] = 0;}}}
+              setup();}
           return false;
      }
       if (y == max && findcolQ (x) < 0){
           removeQueen(y, x);
-          solve(findcolQ (x - 1) + 1, x-1);
+          return solve(findcolQ (x - 1) + 1, x-1);
       }
 
       if (!added){
           if (y == max){
-              solve(0, x +1);
+              return solve(0, x +1);
           }
 
           if (y == 0 && x == 0){
-              solve (0, x +1);
+              return solve (0, x +1);
           }
 
          if ((y != 0 || x !=0) && y != max){
-             solve(y + 1, x);
+             return solve(y + 1, x);
         }
     }
     return false;
@@ -173,8 +215,8 @@ private int findcolQ (int x){
 }
   private boolean checker (){
       int sum = 0;
-      for (int y = 0; y < board.length; y++){
-        for (int x = 0; x < board[y].length; x++){
+      for (int y = 0; y < max; y++){
+        for (int x = 0; x < max; x++){
             if( board [y][x] < 0){
                 sum++;
             }}}
